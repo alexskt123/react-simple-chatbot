0ef6d67188c2948b63181e8dbb18346a1f7b90ad
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2020-10-16 22:05:03 +0800

    radix icons

--- lib/components/CurrentDate.jsx
+++ lib/components/CurrentDate.jsx
@@ -7,10 +7,16 @@ const CurrentDate = () => {
 
   return (
     <Fragment>
-      <div style={{ width: '100%', marginTop: '3px', justifyContent: 'center', display: 'flex', marginBottom: '10px' }}>
-        <span style={{ fontSize: 'x-small', color }}>
-          {nowDate}
-        </span>
+      <div
+        style={{
+          width: '100%',
+          marginTop: '3px',
+          justifyContent: 'center',
+          display: 'flex',
+          marginBottom: '10px'
+        }}
+      >
+        <span style={{ fontSize: 'x-small', color }}>{nowDate}</span>
       </div>
     </Fragment>
   );
