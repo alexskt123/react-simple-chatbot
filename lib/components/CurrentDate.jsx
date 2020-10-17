eeaee01f74b65aadf9d9d36260753cb80423bd0b
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2020-10-17 09:41:48 +0800

    beautify current date

--- lib/components/CurrentDate.jsx
+++ lib/components/CurrentDate.jsx
@@ -3,7 +3,14 @@ import React, { Fragment, useMemo } from 'react';
 const CurrentDate = () => {
   const nowDate = useMemo(() => new Date().toLocaleDateString(), []);
 
-  const color = 'grey';
+  const currentDateConfig = {
+    fontSize: 'x-small',
+    backgroundColor: 'grey',
+    color: 'white',
+    borderRadius: '5px 5px 5px 5px',
+    paddingLeft: '5px',
+    paddingRight: '5px'
+  };
 
   return (
     <Fragment>
@@ -16,7 +23,7 @@ const CurrentDate = () => {
           marginBottom: '10px'
         }}
       >
-        <span style={{ fontSize: 'x-small', color }}>{nowDate}</span>
+        <span style={{ ...currentDateConfig }}>{nowDate}</span>
       </div>
     </Fragment>
   );
